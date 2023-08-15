7b6ee167414e66d93605d3e2d89368d7bc90338a
alexhalcazar <alexhalcazar369@gmail.com>
2023-08-15 16:52:27 -0700

    Got MySQL to work

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,27 @@ import { ApolloServer  } from 'apollo-server-express';
 import { TaskResolver } from './resolvers/task';
 import { buildSchema } from 'type-graphql';
 import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core'
+import { DataSource } from 'typeorm'
 
 const main = async () => {
+
+    const AppDataSource = new DataSource({
+        type: "mysql",
+        host: "localhost",
+        port: 3306,
+        username: "root",
+        password: "Alexbg03",
+        database: "library",
+    })
+
+    AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
+
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [TaskResolver],
